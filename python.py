40313f52f9ee1f16b40a7a0375164b1ed097c355
lblanar <blanar.levente@gmail.com>
2025-04-16 14:28:16 +0200

    adapter tervezesi minta

--- python.py
+++ python.py
@@ -1,6 +1,23 @@
-from locust import task
+# Régi osztály – nem tudjuk módosítani
+class LegacyDataProvider:
+    def get_data(self):
+        return "Régi adat"
 
+# Új rendszer ezt várja:
+class NewSystem:
+    def process(self, source):
+        print("Feldolgozva:", source.fetch())
 
-@task
-def login_user(self):
-    self.client.post("/login", data={"email": "a@b.hu", "password": "123"})
+# Adapter osztály
+class Adapter:
+    def __init__(self, legacy):
+        self.legacy = legacy
+
+    def fetch(self):
+        return self.legacy.get_data()
+
+# Használat
+legacy = LegacyDataProvider()
+adapter = Adapter(legacy)
+uj_rendszer = NewSystem()
+uj_rendszer.process(adapter)
